84919d0c9abe7fa5cbb35e8a952f6bf0d11f9540
bainmatt <52426789+bainmatt@users.noreply.github.com>
2025-04-14 16:01:07 -0400

    ENH: Support rendering of text-embedded links

--- R/autocv-package.R
+++ R/autocv-package.R
@@ -8,7 +8,6 @@
 #' @importFrom here here
 #' @importFrom graphics text
 #' @importFrom magrittr "%>%"
-#' @importFrom tibble add_row
 #' @importFrom rvest read_html
 #' @importFrom usethis use_data
 #' @importFrom withr with_envvar
@@ -17,21 +16,22 @@
 #' @importFrom purrr map_chr reduce
 #' @importFrom tidyselect ends_with
 #' @importFrom pagedown chrome_print
+#' @importFrom tibble add_row tibble
 #' @importFrom stats na.omit reorder
 #' @importFrom assertthat assert_that
 #' @importFrom glue glue glue_collapse
-#' @importFrom stringr str_c str_detect
 #' @importFrom yaml read_yaml write_yaml
 #' @importFrom rlang has_name sym ":=" .data
 #' @importFrom utils read.csv write.csv View
 #' @importFrom fs is_dir is_file path_dir path_rel
+#' @importFrom stringr fixed str_c str_detect str_match_all
 #' @importFrom cli cli_alert_danger cli_alert_success cli_alert_warning
 #' @importFrom ggplot2
 #'  aes coord_flip expand_limits
 #'  element_rect geom_col geom_text labs theme theme_void
 #' @importFrom dplyr
-#'  across arrange case_when c_across desc filter first group_by
-#'  if_else last mutate pull rowwise row_number select starts_with
+#'  across arrange case_when c_across desc everything filter first group_by
+#'  if_else last matches mutate pull rowwise row_number select starts_with
 #'  ungroup where
 ## usethis namespace: end
 NULL
